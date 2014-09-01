a7f78c32d4c7fd22635d9fda1ff9002ebc69189c
Naveen Ramakrishna <naveen.ramakrishna@dhruva.com>
2014-09-01 15:17:46 +0530

    * Added Unity project. * Added the usual Cube rotation code in Unity.

---
+++ Assets/Scripts/RotateMe.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateMe : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(null != gameObject)
+		{
+			gameObject.transform.Rotate(Vector3.forward, 30.0f * Time.deltaTime) ;
+			gameObject.transform.Rotate(Vector3.up, -30.0f * Time.deltaTime) ;
+			gameObject.transform.Rotate(Vector3.left, 30.0f * Time.deltaTime) ;
+		}
+	}
+}
